e72e43d539f0703b682e5f2caafdc3dee6a48529
alessandro-001 <alessandrofrondini@gmail.com>
2023-05-30 14:03:25 +0100

    saving before branching

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
+import Home from './components/Home'
+import Nav from './components/Nav'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 function App() {
 
   return (
-    <>
-    </>
+  <BrowserRouter>
+    <main className='App'>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} /> 
+        
+      </Routes>
+    </main>
+  </BrowserRouter>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home () {
+    return (
+        <header>
+            <h1> Welcome to NC news</h1>
+            <p>Please use the navigation bar to access articles</p>
+        </header>
+    )
+}
+
+export default Home;
